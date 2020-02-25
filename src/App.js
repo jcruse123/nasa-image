280aad1322c8b9aae793325baa4ae8700dede2b2
Joshua Cruse <jcruse123@gmail.com>
2020-02-25 10:12:36 -0700

    minor fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import Container from 'react-bootstrap/Container'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import PhotoDashboard from './components/PhotoDashboard'
